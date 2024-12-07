eb048bfc71cd16762ecbc1f4f940f9e3503a788f
konradsoares <konrad.soares@gmail.com>
2024-12-07 21:16:28 +0000

    original to dev

--- SeleniumTest/SeleniumTest.cs
+++ SeleniumTest/SeleniumTest.cs
@@ -1,64 +1,35 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-// NuGet install Selenium WebDriver package and Support Classes
- 
 using OpenQA.Selenium;
-
-// NuGet install Chrome Driver
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
-
-// run 2 instances of VS to do run Selenium tests against localhost
-// instance 1 : run web app e.g. on IIS Express
-// instance 2 : from Test Explorer run Selenium test
-// or use the dotnet vstest task
-// e.g. dotnet vstest SeleniumTest\bin\debug\netcoreapp2.1\seleniumtest.dll /Settings:SeleniumTest.runsettings
 
 namespace SeleniumTest
 {
     [TestClass]
     public class UnitTest1
     {
-        // .runsettings file contains test run parameters
-        // e.g. URI for app
-        // test context for this run
-
         private TestContext testContextInstance;
 
-        // test harness uses this property to initliase test context
         public TestContext TestContext
         {
             get { return testContextInstance; }
             set { testContextInstance = value; }
         }
 
-        // URI for web app being tested
         private String webAppUri;
 
-        // .runsettings property overriden in vsts test runner 
-        // release task to point to run settings file
-        // also webAppUri overriden to use pipeline variable
-
-        [TestInitialize]                // run before each unit test
+        [TestInitialize]
         public void Setup()
         {
-            // read URL from SeleniumTest.runsettings (configure run settings)
-            //this.webAppUri = testContextInstance.Properties["webAppUri"].ToString();
-
-            this.webAppUri = "https://bp-ca1-ks-final-dev-efbdh3a8hfgufufu.germanywestcentral-01.azurewebsites.net/";
+            this.webAppUri = "https://bp-ca1-ks-finalendpoint-epfhcxa8f7brb2fn.z01.azurefd.net/";
         }
 
         [TestMethod]
         public void TestBP()
         {
+            String chromeDriverPath = Environment.GetEnvironmentVariable("ChromeWebDriver") ?? ".";
 
-            String chromeDriverPath = Environment.GetEnvironmentVariable("ChromeWebDriver");
-            if (chromeDriverPath is null)
-            {
-                chromeDriverPath = ".";                 // for IDE
-            }
-          
             using (IWebDriver driver = new ChromeDriver(chromeDriverPath))
             {
                 // Navigate to the web app
@@ -66,10 +37,12 @@ namespace SeleniumTest
 
                 // Enter systolic and diastolic values
                 IWebElement SystolicElement = driver.FindElement(By.Id("BP_Systolic"));
-                SystolicElement.SendKeys("110");  // Ideal BP systolic value (adjusted for "Ideal" test)
+                SystolicElement.Clear(); // Clear existing value
+                SystolicElement.SendKeys("110");  // Ideal BP systolic value
 
                 IWebElement DiastolicElement = driver.FindElement(By.Id("BP_Diastolic"));
-                DiastolicElement.SendKeys("75");  // Ideal BP diastolic value (adjusted for "Ideal" test)
+                DiastolicElement.Clear(); // Clear existing value
+                DiastolicElement.SendKeys("75");  // Ideal BP diastolic value
 
                 // Submit the form
                 driver.FindElement(By.CssSelector(".btn")).Submit();
@@ -82,11 +55,9 @@ namespace SeleniumTest
                 string bpResult = BPValueElement.GetAttribute("value");
 
                 // Validate the result
-                StringAssert.EndsWith(bpResult, "Ideal");  // Since the input value is "Ideal"
+                StringAssert.EndsWith(bpResult, "Ideal");
 
                 driver.Quit();
-
-                // alternative - use Cypress or Playright
             }
         }
     }
