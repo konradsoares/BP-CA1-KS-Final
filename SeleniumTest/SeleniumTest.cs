21e5ac5a6dac097b33ffe7b8492fbc37aac93aee
konradsoares <konrad.soares@gmail.com>
2024-12-07 22:01:55 +0000

    prod selenium fix

--- SeleniumTest/SeleniumTest.cs
+++ SeleniumTest/SeleniumTest.cs
@@ -55,7 +55,7 @@ namespace SeleniumTest
                 string bpResult = BPValueElement.GetAttribute("value");
 
                 // Validate the result
-                StringAssert.EndsWith(bpResult, "Ideal");
+                StringAssert.EndsWith(bpResult, "Ideal Blood Pressure");
 
                 driver.Quit();
             }
